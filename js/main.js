5c642a8da6944268bc8d8e4195fd7ac54b003054
Va-leri <v.budilova@mail.ru>
2019-10-10 12:17:19 +0300

    Личный проект: надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 'use strict';
 
-// Вычисление координат круглой метки
-// var getMapPinMainWidth = function () {
-//   return mapPinMain.offsetWidth;
-// };
-// var getMapPinMainHeight = function () {
-//   return mapPinMain.offsetHeight;
-// };
+// (function () {
+//   window.main = {
+//     showError: function () {
+//       // Находим шаблон ошибки .error из шаблона #error
+//       var errorTemplate = document.querySelector('#error').content.querySelector('.error');
 
-// Форма - adForm
+//       // Находим блок main в разметке
+//       var mainBlock = document.querySelector('main');
+
+//       var errorBlock = errorTemplate.cloneNode(true);
+
+//       // Вставляем блок с ошибкой в разметку (в начало блока main)
+//       mainBlock.prepend(errorBlock);
+//     }
+//   };
+// })();
+// var showError = function () {
+//   // Находим шаблон ошибки .error из шаблона #error
+//   var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+
+//   // Находим блок main в разметке
+//   var mainBlock = document.querySelector('main');
+
+//   var errorBlock = errorTemplate.cloneNode(true);
+
+//   // Вставляем блок с ошибкой в разметку (в начало блока main)
+//   mainBlock.prepend(errorBlock);
+// };
